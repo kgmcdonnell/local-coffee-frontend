6b5463c6e4aa665209cdfdd3d9e679b961daec0f
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-30 17:34:00 -0400

    implemented search bar in the coffee shops component

--- src/CoffeeShopsIndex.jsx
+++ src/CoffeeShopsIndex.jsx
@@ -1,32 +1,43 @@
+import { useState } from "react";
 import "./CoffeeShopsIndex.scoped.scss";
 
 export function CoffeeShopsIndex(props) {
+  const [search, setSearch] = useState("");
   return (
     <div className="coffee-shops-index">
       <div className="row">
         <div className="col-sm-5">
-          <h3 className="text-center" style={{ marginBottom: "20px" }}>
-            Local Coffee Shops
-          </h3>
+          <div className="input-group mb-3 search">
+            <input
+              type="text"
+              className="form-control"
+              value={search}
+              onChange={event => setSearch(event.target.value)}
+              placeholder="search"
+            ></input>
+          </div>
+
           <div>
             {props?.coffeeShops && props.coffeeShops.length > 0 ? (
-              props.coffeeShops.map(shop => (
-                <div key={shop.place_id}>
-                  <div className="row">
-                    <div className="child-row">
-                      <p>
-                        <strong>{shop.name}</strong>
-                      </p>
-                      <p>
-                        <small>Address: {shop.formatted_address}</small>
-                      </p>
-                      <p>
-                        <small>Rating: {shop.rating}</small>
-                      </p>
+              props.coffeeShops
+                .filter(shop => shop.name.toLowerCase().includes(search.toLowerCase()))
+                .map(shop => (
+                  <div key={shop.place_id}>
+                    <div className="row">
+                      <div className="child-row">
+                        <p>
+                          <strong>{shop.name}</strong>
+                        </p>
+                        <p>
+                          <small>Address: {shop.formatted_address}</small>
+                        </p>
+                        <p>
+                          <small>Rating: {shop.rating}</small>
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))
+                ))
             ) : (
               <></>
             )}
