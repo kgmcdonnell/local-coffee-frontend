955c3c6e18fef19cab6ba9974ca5b00a3d0cdc41
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-05-01 13:08:00 -0400

    added a search button with a magnifying glass icon

--- src/CoffeeShopsIndex.jsx
+++ src/CoffeeShopsIndex.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./CoffeeShopsIndex.scoped.scss";
 
 export function CoffeeShopsIndex(props) {
-  const [search, setSearch] = useState("");
   return (
     <div className="coffee-shops-index">
       <div className="row">
@@ -10,31 +10,32 @@ export function CoffeeShopsIndex(props) {
           <div className="search input-group">
             <input className="form-control input" type="text" placeholder="City" />
             <input className="form-control input" type="text" placeholder="State" />
+            <button type="submit" style={{ backgroundColor: "transparent", border: "none" }}>
+              <FontAwesomeIcon className="fa-magnifying-glass" icon={faMagnifyingGlass} style={{ margin: "10px" }} />
+            </button>
           </div>
           <div>
             {props?.coffeeShops && props.coffeeShops.length > 0 ? (
-              props.coffeeShops
-                .filter(shop => shop.name.toLowerCase().includes(search.toLowerCase()))
-                .map(shop => (
-                  <div key={shop.place_id}>
-                    <div className="row">
-                      <div className="child-row">
-                        <p>
-                          <strong>{shop.name}</strong>
-                        </p>
-                        <p>
-                          <small>Address: {shop.formatted_address}</small>
-                        </p>
-                        <p>
-                          <small>Rating: {shop.rating}</small>
-                        </p>
-                        <button type="button" className="btn btn-dark" style={{ marginTop: "10px" }}>
-                          More Info
-                        </button>
-                      </div>
+              props.coffeeShops.map(shop => (
+                <div key={shop.place_id}>
+                  <div className="row">
+                    <div className="child-row">
+                      <p>
+                        <strong>{shop.name}</strong>
+                      </p>
+                      <p>
+                        <small>Address: {shop.formatted_address}</small>
+                      </p>
+                      <p>
+                        <small>Rating: {shop.rating}</small>
+                      </p>
+                      <button type="button" className="btn btn-dark" style={{ marginTop: "10px" }}>
+                        More Info
+                      </button>
                     </div>
                   </div>
-                ))
+                </div>
+              ))
             ) : (
               <></>
             )}
