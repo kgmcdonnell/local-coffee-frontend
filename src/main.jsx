cadbe4e6b0459a142318130edd3801f71b81b638
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-06-22 09:43:38 -0400

    updated onrender backened site in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://local-coffee-api.onrender.com";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://roastd.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
